291a7afca4ba34e9b34b989074c717bacbaba897
Andrey <kotov.andrey@mail.ru>
2022-12-21 22:36:09 +0300

    change

--- src/main/webapp/MyServlet/MainServlet.java
+++ src/main/webapp/MyServlet/MainServlet.java
@@ -1,4 +1,4 @@
-
+package MyServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-@WebServlet("/WELCOME")
+
 public class MainServlet extends HttpServlet {
 
     @Override
